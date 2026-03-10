9e1f5ed553cab7723c885c220f23ec36413459a5
lyodine <commit@lyodine.com>
2026-03-10 16:14:51 +0800

    code(midi): Move large classes to end of file

--- omusic/midi.py
+++ omusic/midi.py
@@ -22,203 +22,6 @@ type RangeInclusive = tuple[int, int]
 type Channel = Annotated[int, RangeInclusive[0, 15]]
 
 
-class Instrument(IntEnum):
-    """Instrument codes implemented with
-    reference to the General MIDI Level 1 specification (`source`_).
-
-    .. _source: https://midi.org/general-midi-level-1
-    """
-    AcousticGrandPiano = 1
-    BrightAcousticPiano = 2
-    ElectricGrandPiano = 3
-    HonkyTonkPiano = 4
-    ElectricPiano1 = 5
-    ElectricPiano2 = 6
-    Harpsichord = 7
-    Clavi = 8
-    Celesta = 9
-    Glockenspiel = 10
-    MusicBox = 11
-    Vibraphone = 12
-    Marimba = 13
-    Xylophone = 14
-    TubularBells = 15
-    Dulcimer = 16
-    DrawbarOrgan = 17
-    PercussiveOrgan = 18
-    RockOrgan = 19
-    ChurchOrgan = 20
-    ReedOrgan = 21
-    Accordion = 22
-    Harmonica = 23
-    TangoAccordion = 24
-    AcousticGuitarNylon = 25
-    AcousticGuitarSteel = 26
-    ElectricGuitarJazz = 27
-    ElectricGuitarClean = 28
-    ElectricGuitarMuted = 29
-    OverdrivenGuitar = 30
-    DistortionGuitar = 31
-    Guitarharmonics = 32
-    AcousticBass = 33
-    ElectricBassFinger = 34
-    ElectricBassPick = 35
-    FretlessBass = 36
-    SlapBass1 = 37
-    SlapBass2 = 38
-    SynthBass1 = 39
-    SynthBass2 = 40
-    Violin = 41
-    Viola = 42
-    Cello = 43
-    Contrabass = 44
-    TremoloStrings = 45
-    PizzicatoStrings = 46
-    OrchestralHarp = 47
-    Timpani = 48
-    StringEnsemble1 = 49
-    StringEnsemble2 = 50
-    SynthStrings1 = 51
-    SynthStrings2 = 52
-    ChoirAahs = 53
-    VoiceOohs = 54
-    SynthVoice = 55
-    OrchestraHit = 56
-    Trumpet = 57
-    Trombone = 58
-    Tuba = 59
-    MutedTrumpet = 60
-    FrenchHorn = 61
-    BrassSection = 62
-    SynthBrass1 = 63
-    SynthBrass2 = 64
-    SopranoSax = 65
-    AltoSax = 66
-    TenorSax = 67
-    BaritoneSax = 68
-    Oboe = 69
-    EnglishHorn = 70
-    Bassoon = 71
-    Clarinet = 72
-    Piccolo = 73
-    Flute = 74
-    Recorder = 75
-    PanFlute = 76
-    BlownBottle = 77
-    Shakuhachi = 78
-    Whistle = 79
-    Ocarina = 80
-    Lead1Square = 81
-    Lead2Sawtooth = 82
-    Lead3Calliope = 83
-    Lead4Chiff = 84
-    Lead5Charang = 85
-    Lead6Voice = 86
-    Lead7Fifths = 87
-    Lead8BassPlusLead = 88
-    Pad1Newage = 89
-    Pad2Warm = 90
-    Pad3Polysynth = 91
-    Pad4Choir = 92
-    Pad5Bowed = 93
-    Pad6Metallic = 94
-    Pad7Halo = 95
-    Pad8Sweep = 96
-    FX1Rain = 97
-    FX2Soundtrack = 98
-    FX3Crystal = 99
-    FX4Atmosphere = 100
-    FX5Brightness = 101
-    FX6Goblins = 102
-    FX7Echoes = 103
-    FX8SciFi = 104
-    Sitar = 105
-    Banjo = 106
-    Shamisen = 107
-    Koto = 108
-    Kalimba = 109
-    Bagpipe = 110
-    Fiddle = 111
-    Shanai = 112
-    TinkleBell = 113
-    Agogo = 114
-    SteelDrums = 115
-    Woodblock = 116
-    TaikoDrum = 117
-    MelodicTom = 118
-    SynthDrum = 119
-    ReverseCymbal = 120
-    GuitarFretNoise = 121
-    BreathNoise = 122
-    Seashore = 123
-    BirdTweet = 124
-    TelephoneRing = 125
-    Helicopter = 126
-    Applause = 127
-    Gunshot = 128
-
-
-@dataclass(frozen=True)
-class Percussion():
-    """Percussion instrument codes implemented with
-    reference to the General MIDI Level 1 specification (`source`_).
-
-    Use these values as notes and not as instruments.
-    On channel 10 (or 9 because channels in MIDI messages are
-    zero-indexed), notes are interpreted as percussion
-    sounds.
-
-    .. _source: https://midi.org/general-midi-level-1
-    """
-    AcousticBassDrum = 35
-    BassDrum = 36
-    SideStick = 37
-    AcousticSnare = 38
-    HandClap = 39
-    ElectricSnare = 40
-    LowFloorTom = 41
-    ClosedHiHat = 42
-    HighFloorTom = 43
-    PedalHiHat = 44
-    LowTom = 45
-    OpenHiHat = 46
-    LowMidTom = 47
-    HiMidTom = 48
-    CrashCymbal = 49
-    HighTom = 50
-    RideCymbal = 51
-    ChineseCymbal = 52
-    RideBell = 53
-    Tambourine = 54
-    SplashCymbal = 55
-    Cowbell = 56
-    CrashCymbal = 57
-    Vibraslap = 58
-    RideCymbal = 59
-    HiBongo = 60
-    LowBongo = 61
-    MuteHiConga = 62
-    OpenHiConga = 63
-    LowConga = 64
-    HighTimbale = 65
-    LowTimbale = 66
-    HighAgogo = 67
-    LowAgogo = 68
-    Cabasa = 69
-    Maracas = 70
-    ShortWhistle = 71
-    LongWhistle = 72
-    ShortGuiro = 73
-    LongGuiro = 74
-    Claves = 75
-    HiWoodBlock = 76
-    LowWoodBlock = 77
-    MuteCuica = 78
-    OpenCuica = 79
-    MuteTriangle = 80
-    OpenTriangle = 81
-
-
 # Suppressing because MIDO does not come
 #   with the right stubs.
 def show_ports() -> list[str]:
@@ -423,3 +226,200 @@ def change_instrument(player: Player,
     player.port.send(mido.Message('program_change',
                                   channel=channel,
                                   program=instrument))
+
+
+class Instrument(IntEnum):
+    """Instrument codes implemented with
+    reference to the General MIDI Level 1 specification (`source`_).
+
+    .. _source: https://midi.org/general-midi-level-1
+    """
+    AcousticGrandPiano = 1
+    BrightAcousticPiano = 2
+    ElectricGrandPiano = 3
+    HonkyTonkPiano = 4
+    ElectricPiano1 = 5
+    ElectricPiano2 = 6
+    Harpsichord = 7
+    Clavi = 8
+    Celesta = 9
+    Glockenspiel = 10
+    MusicBox = 11
+    Vibraphone = 12
+    Marimba = 13
+    Xylophone = 14
+    TubularBells = 15
+    Dulcimer = 16
+    DrawbarOrgan = 17
+    PercussiveOrgan = 18
+    RockOrgan = 19
+    ChurchOrgan = 20
+    ReedOrgan = 21
+    Accordion = 22
+    Harmonica = 23
+    TangoAccordion = 24
+    AcousticGuitarNylon = 25
+    AcousticGuitarSteel = 26
+    ElectricGuitarJazz = 27
+    ElectricGuitarClean = 28
+    ElectricGuitarMuted = 29
+    OverdrivenGuitar = 30
+    DistortionGuitar = 31
+    Guitarharmonics = 32
+    AcousticBass = 33
+    ElectricBassFinger = 34
+    ElectricBassPick = 35
+    FretlessBass = 36
+    SlapBass1 = 37
+    SlapBass2 = 38
+    SynthBass1 = 39
+    SynthBass2 = 40
+    Violin = 41
+    Viola = 42
+    Cello = 43
+    Contrabass = 44
+    TremoloStrings = 45
+    PizzicatoStrings = 46
+    OrchestralHarp = 47
+    Timpani = 48
+    StringEnsemble1 = 49
+    StringEnsemble2 = 50
+    SynthStrings1 = 51
+    SynthStrings2 = 52
+    ChoirAahs = 53
+    VoiceOohs = 54
+    SynthVoice = 55
+    OrchestraHit = 56
+    Trumpet = 57
+    Trombone = 58
+    Tuba = 59
+    MutedTrumpet = 60
+    FrenchHorn = 61
+    BrassSection = 62
+    SynthBrass1 = 63
+    SynthBrass2 = 64
+    SopranoSax = 65
+    AltoSax = 66
+    TenorSax = 67
+    BaritoneSax = 68
+    Oboe = 69
+    EnglishHorn = 70
+    Bassoon = 71
+    Clarinet = 72
+    Piccolo = 73
+    Flute = 74
+    Recorder = 75
+    PanFlute = 76
+    BlownBottle = 77
+    Shakuhachi = 78
+    Whistle = 79
+    Ocarina = 80
+    Lead1Square = 81
+    Lead2Sawtooth = 82
+    Lead3Calliope = 83
+    Lead4Chiff = 84
+    Lead5Charang = 85
+    Lead6Voice = 86
+    Lead7Fifths = 87
+    Lead8BassPlusLead = 88
+    Pad1Newage = 89
+    Pad2Warm = 90
+    Pad3Polysynth = 91
+    Pad4Choir = 92
+    Pad5Bowed = 93
+    Pad6Metallic = 94
+    Pad7Halo = 95
+    Pad8Sweep = 96
+    FX1Rain = 97
+    FX2Soundtrack = 98
+    FX3Crystal = 99
+    FX4Atmosphere = 100
+    FX5Brightness = 101
+    FX6Goblins = 102
+    FX7Echoes = 103
+    FX8SciFi = 104
+    Sitar = 105
+    Banjo = 106
+    Shamisen = 107
+    Koto = 108
+    Kalimba = 109
+    Bagpipe = 110
+    Fiddle = 111
+    Shanai = 112
+    TinkleBell = 113
+    Agogo = 114
+    SteelDrums = 115
+    Woodblock = 116
+    TaikoDrum = 117
+    MelodicTom = 118
+    SynthDrum = 119
+    ReverseCymbal = 120
+    GuitarFretNoise = 121
+    BreathNoise = 122
+    Seashore = 123
+    BirdTweet = 124
+    TelephoneRing = 125
+    Helicopter = 126
+    Applause = 127
+    Gunshot = 128
+
+
+@dataclass(frozen=True)
+class Percussion():
+    """Percussion instrument codes implemented with
+    reference to the General MIDI Level 1 specification (`source`_).
+
+    Use these values as notes and not as instruments.
+    On channel 10 (or 9 because channels in MIDI messages are
+    zero-indexed), notes are interpreted as percussion
+    sounds.
+
+    .. _source: https://midi.org/general-midi-level-1
+    """
+    AcousticBassDrum = 35
+    BassDrum = 36
+    SideStick = 37
+    AcousticSnare = 38
+    HandClap = 39
+    ElectricSnare = 40
+    LowFloorTom = 41
+    ClosedHiHat = 42
+    HighFloorTom = 43
+    PedalHiHat = 44
+    LowTom = 45
+    OpenHiHat = 46
+    LowMidTom = 47
+    HiMidTom = 48
+    CrashCymbal = 49
+    HighTom = 50
+    RideCymbal = 51
+    ChineseCymbal = 52
+    RideBell = 53
+    Tambourine = 54
+    SplashCymbal = 55
+    Cowbell = 56
+    CrashCymbal = 57
+    Vibraslap = 58
+    RideCymbal = 59
+    HiBongo = 60
+    LowBongo = 61
+    MuteHiConga = 62
+    OpenHiConga = 63
+    LowConga = 64
+    HighTimbale = 65
+    LowTimbale = 66
+    HighAgogo = 67
+    LowAgogo = 68
+    Cabasa = 69
+    Maracas = 70
+    ShortWhistle = 71
+    LongWhistle = 72
+    ShortGuiro = 73
+    LongGuiro = 74
+    Claves = 75
+    HiWoodBlock = 76
+    LowWoodBlock = 77
+    MuteCuica = 78
+    OpenCuica = 79
+    MuteTriangle = 80
+    OpenTriangle = 81
